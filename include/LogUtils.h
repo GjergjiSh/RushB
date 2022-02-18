449222a4b6c61ab47c9d665e8c98480a7b258d88
GjergjiSh <gjergji_shkurti@yahoo.com>
2022-02-18 13:45:32 +0100

    fixed wrong name in log utils

--- include/LogUtils.h
+++ include/LogUtils.h
@@ -5,14 +5,14 @@
 #define NO_VERB false
 
 #define LOG_INFO(msg) \
-    std::cout << "[I][Spider] " << msg << std::endl;
+    std::cout << "[I][ModuleHandler] " << msg << std::endl;
 #define LOG_TIME_INFO(msg, time) \
-    std::cerr << "[I][Spider] " << msg << " [" << time << " ms]" << std::endl;
+    std::cerr << "[I][ModuleHandler] " << msg << " [" << time << " ms]" << std::endl;
 #define LOG_WARNING(msg) \
-    std::cerr << "[W][Spider] " << msg << std::endl;
+    std::cerr << "[W][ModuleHandler] " << msg << std::endl;
 #define LOG_ERROR_DESCRIPTION(msg, err) \
-    std::cerr << "[E][Spider] " << msg << " " << err << std::endl;
+    std::cerr << "[E][ModuleHandler] " << msg << " " << err << std::endl;
 #define LOG_ERROR(msg) \
-    std::cerr << "[E][Spider] " << msg << std::endl;
+    std::cerr << "[E][ModuleHandler] " << msg << std::endl;
 
 #endif /* COMMON_H */
