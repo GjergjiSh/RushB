d3a2a6701156d6647fd898a0118d0c7ec2e74c0f
GjergjiSh <gjergji.shkurti@steinbeis-ias.de>
2022-02-21 14:30:39 +0100

    added video pub module

---
+++ modules/videopub/include/VideoPub.h
@@ -0,0 +1,45 @@
+#include "ModuleInterface.h"
+
+#include <gst/gst.h>
+#include <memory>
+
+#define LOG_INFO(msg) \
+    std::cout << "[I][VideoPub] " << msg << std::endl;
+#define LOG_TIME_INFO(msg, time) \
+    std::cerr << "[I][VideoPub] " << msg << " [" << time << " ms]" << std::endl;
+#define LOG_WARNING(msg) \
+    std::cerr << "[W][VideoPub] " << msg << std::endl;
+#define LOG_ERROR_DESCRIPTION(msg, err) \
+    std::cerr << "[E][VideoPub] " << msg << " " << err << std::endl;
+#define LOG_ERROR(msg) \
+    std::cerr << "[E][VideoPub] " << msg << std::endl;
+
+typedef struct
+{
+    GstElement* pipe;
+    GstElement* v4l2src;
+    GstElement* videoconvert;
+    GstElement* x264enc;
+    GstElement* rtph264pay;
+    GstElement* udpsink;
+} VideoPipeline_t;
+
+
+class VideoPub : public Module {
+
+public:
+    VideoPub();
+    int Init(void) override;
+    int Cycle_Step(void) override;
+    int Deinit(void) override;
+
+private:
+    int Construct_Pipeline();
+    int Destroy_Pipeline();
+    int Set_Pipeline_State_Playing();
+    int Create_Elements();
+    int Configure_Elements();
+    int Link_Elements();
+
+    std::shared_ptr<VideoPipeline_t> pipeline;
+};
---
+++ modules/videopub/src/VideoPub.cpp
@@ -0,0 +1,122 @@
+#include "VideoPub.h"
+
+VideoPub::VideoPub() {
+    this->name="VideoPub";
+}
+
+int VideoPub::Init()
+{
+    LOG_INFO("Initializing...");
+
+    if (Construct_Pipeline() != 0) return -1;
+    if (Set_Pipeline_State_Playing() != 0) return -1;
+
+    return 0;
+}
+
+int VideoPub::Cycle_Step()
+{
+    return 0;
+}
+
+int VideoPub::Deinit()
+{
+    LOG_INFO("Deinitializing...");
+    return Destroy_Pipeline();
+}
+
+int VideoPub::Construct_Pipeline()
+{
+    this->pipeline = std::make_shared<VideoPipeline_t>();
+
+    gst_init(NULL, NULL);
+    if (Create_Elements() != 0) return -1;
+    if (Configure_Elements() != 0) return -1;
+    if (Link_Elements() != 0) return -1;
+
+    LOG_INFO("Video pipeline successfuly constructed");
+    return 0;
+}
+
+int VideoPub::Create_Elements()
+{
+    this->pipeline->pipe = gst_pipeline_new("Publisher Video Pipeline");
+    this->pipeline->v4l2src = gst_element_factory_make("v4l2src", NULL);
+    this->pipeline->videoconvert = gst_element_factory_make("videoconvert", NULL);
+    this->pipeline->x264enc = gst_element_factory_make("x264enc", NULL);
+    this->pipeline->rtph264pay = gst_element_factory_make("rtph264pay", NULL);
+    this->pipeline->udpsink = gst_element_factory_make("udpsink", NULL);
+
+    if (!pipeline->pipe ||
+        !this->pipeline->v4l2src ||
+        !this->pipeline->videoconvert ||
+        !this->pipeline->x264enc ||
+        !this->pipeline->rtph264pay ||
+        !this->pipeline->udpsink) {
+
+        LOG_ERROR("Elements could not be created");
+        return -1;
+    }
+
+    gst_bin_add_many(GST_BIN(pipeline->pipe),
+        this->pipeline->v4l2src,
+        this->pipeline->videoconvert,
+        this->pipeline->x264enc,
+        this->pipeline->rtph264pay,
+        this->pipeline->udpsink, NULL);
+
+    return 0;
+}
+
+int VideoPub::Configure_Elements()
+{
+    g_object_set(pipeline->v4l2src, "device", parameters.at("CAMERA_INDEX").c_str(), NULL);
+    g_object_set(pipeline->x264enc, "tune", 4, NULL);
+    g_object_set(pipeline->x264enc, "speed-preset", 1, NULL);
+    g_object_set(pipeline->x264enc, "bitrate", std::stoi(parameters.at("BITRATE")), NULL);
+    g_object_set(G_OBJECT(pipeline->udpsink), "host", parameters.at("REMOTE_HOST").c_str(), NULL);
+    g_object_set(G_OBJECT(pipeline->udpsink), "port", std::stoi(parameters.at("OUT_PORT")), NULL);
+
+    return 0;
+}
+
+int VideoPub::Link_Elements()
+{
+    if (!gst_element_link_many(
+        pipeline->v4l2src,
+        pipeline->videoconvert,
+        pipeline->x264enc,
+        pipeline->rtph264pay,
+        pipeline->udpsink, NULL)) {
+        LOG_ERROR("Elements could not be linked");
+        gst_object_unref(pipeline->pipe);
+        return -1;
+    }
+    return 0;
+}
+
+int VideoPub::Destroy_Pipeline()
+{
+    gst_element_set_state(this->pipeline->pipe, GST_STATE_NULL);
+    gst_object_unref(GST_OBJECT(this->pipeline->pipe));
+
+    LOG_INFO("Video pipeline destroyed");
+    return 0;
+}
+
+int VideoPub::Set_Pipeline_State_Playing()
+{
+    GstStateChangeReturn ret = gst_element_set_state(pipeline->pipe, GST_STATE_PLAYING);
+    if (ret == GST_STATE_CHANGE_FAILURE) {
+        LOG_ERROR("Unable to set the pipeline to the playing state");
+        gst_object_unref(pipeline->pipe);
+        return -1;
+    } else {
+        LOG_INFO("Video pipeline set to playing");
+    }
+    return 0;
+}
+
+
+extern "C" Module* Create() { return new VideoPub;}
+extern "C" void Destroy(Module* module) {delete module;}
