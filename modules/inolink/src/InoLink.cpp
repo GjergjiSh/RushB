77f9780c97d35318300f650866efe06dc41502de
GjergjiSh <gjergji.shkurti@steinbeis-ias.de>
2022-02-25 16:10:23 +0100

    configurable baud rate for inolink

--- modules/inolink/src/InoLink.cpp
+++ modules/inolink/src/InoLink.cpp
@@ -31,8 +31,9 @@ int InoLink::Init_Serial_Port()
 {
     try {
         std::string port = parameters.at("SERIAL_PORT");
-        serial_port = new mn::CppLinuxSerial::SerialPort(port,
-            mn::CppLinuxSerial::BaudRate::B_460800);
+        serial_port = new mn::CppLinuxSerial::SerialPort();
+        serial_port->SetDevice(port);
+        serial_port->SetBaudRate(std::stoi(parameters.at("BAUD_RATE")));
 
         serial_port->SetTimeout(-1);
         serial_port->Open();
@@ -63,20 +64,21 @@ std::string InoLink::Convert_Servo_Vals()
     int c_right_servo = ((shared_data->servos.right_servo + 100) * 90) / 100;
     int c_top_servo = ((shared_data->servos.top_servo + 100) * 90) / 100;
 
-    std::string left_servo = std::to_string(c_left_servo) + "%";
+    std::string left_servo = std::to_string(c_left_servo) + "@";
     std::string right_servo = std::to_string(c_right_servo) + "#";
-    std::string top_servo = std::to_string(c_top_servo) + "&";
-    std::string output_servo_string = left_servo + right_servo + top_servo;
+    std::string top_servo = std::to_string(c_top_servo) + "$";
 
-    std::cout << output_servo_string << std::endl;
-
+    std::string output_servo_string = "!"+ left_servo
+                                         + right_servo
+                                         + top_servo +"\n";
     return output_servo_string;
 }
 
 int InoLink::Write_Servo_Vals()
 {
     try {
-        serial_port->Write("!" + Convert_Servo_Vals() + "\n");
+        const std::string output_servo_string =  Convert_Servo_Vals();
+        serial_port->Write(output_servo_string);
     } catch (mn::CppLinuxSerial::Exception& ex) {
         LOG_ERROR_DESCRIPTION("Failed to write driver wishes to the robot.", ex.what());
         return -1;
