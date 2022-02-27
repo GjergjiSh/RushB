55e3e5cbb5b20d4f0b07e26548c98dbf31a9c86f
GjergjiSh <gjergji.shkurti@steinbeis-ias.de>
2022-02-27 12:54:33 +0100

    updated cmakelists for example modules

--- example-modules/reader/src/Reader.cpp
+++ example-modules/reader/src/Reader.cpp
@@ -1,17 +1,17 @@
 #include "Reader.h"
 
-
-Reader::Reader() {
-    name="Reader";
+Reader::Reader()
+{
+    name = "Reader";
     logger.Set_Name(name);
 }
 
 int Reader::Init()
 {
     try {
-    logger.LOG_INFO(std::string("Reder says: ").append(parameters.at("HELLO")));
+        logger.LOG_INFO(std::string("Reader says: ").append(parameters.at("HELLO")));
 
-    } catch(std::exception& e) {
+    } catch (std::exception& e) {
         logger.LOG_ERROR_DESCRIPTION("Failed to read parameter: ", e.what());
         return -1;
     }
@@ -20,9 +20,9 @@ int Reader::Init()
 
 int Reader::Cycle_Step()
 {
-    logger.LOG_INFO(std::string("Top Servo: ").append(parameters.at(std::to_string(shared_data->servos.top_servo))));
-    logger.LOG_INFO(std::string("Left Servo: ").append(parameters.at(std::to_string(shared_data->servos.left_servo))));
-    logger.LOG_INFO(std::string("Right Servo: ").append(parameters.at(std::to_string(shared_data->servos.right_servo))));
+    logger.LOG_INFO(std::string("Top Servo: ").append((std::to_string(shared_data->servos.top_servo))));
+    logger.LOG_INFO(std::string("Left Servo: ").append((std::to_string(shared_data->servos.left_servo))));
+    logger.LOG_INFO(std::string("Right Servo: ").append((std::to_string(shared_data->servos.right_servo))));
 
     return 0;
 }
@@ -32,5 +32,5 @@ int Reader::Deinit()
     return 0;
 }
 
-extern "C" Module* Create() { return new Reader;}
-extern "C" void Destroy(Module* module) {delete module;}
+extern "C" Module* Create() { return new Reader; }
+extern "C" void Destroy(Module* module) { delete module; }
